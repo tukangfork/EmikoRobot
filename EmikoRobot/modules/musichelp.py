21bbf389160e6da3df3892d4899c6d4dd5a20da1
tukangfork <102410053+tukangfork@users.noreply.github.com>
2022-03-27 06:59:33 +0700

    Update musichelp.py

--- EmikoRobot/modules/musichelp.py
+++ EmikoRobot/modules/musichelp.py
@@ -1,19 +1,36 @@
 __mod_name__ = "Musicplayer"
 
 __help__ = """
-──「 Music stream 」──              
+Available Commands = play , vplay , cplay
+ForcePlay Commands = playforce , vplayforce , cplayforce
 
-❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
-❂ /playlist (choose option) for playing your personal playlist or group playlist.
+c stands for channel play.
+v stands for video play.
+force stands for force play.
+
+❂ /play or /vplay or /cplay (query or reply audio) - - Bot will start playing your given query on voice chat or Stream live links on voice chats.
+❂ /playforce or /vplayforce or /cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
+❂ /channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
+❂ /playlist - Check Your Saved Playlist On Servers.
+❂ /deleteplaylist - Delete any saved music in your playlist
+❂ /play  - Start playing Your Saved Playlist from Servers.
 
 • Admins only
+c stands for channel play.
 
-❂ /auth - Add to authorized users.
-❂ /unauth - Canceling or deleting from authorized users list.
-❂ /msettings - To open panel settings on musicplayer.
-❂ /setadmin - To set all command only for admins.
-❂ /setmember - Set all command for all members.
-❂ /vol (range 1-200) - To set volume on voice calls.
+❂ /pause or /cpause - Pause the playing music.
+❂ /resume or /cresume- Resume the paused music.
+❂ /mute or /cmute- Mute the playing music
+❂ /unmute or /cunmute- Unmute the muted music
+❂ /shuffle or /cshuffle- Randomly shuffles the queued playlist.
+❂ /seek or /cseek - Forward Seek the music to your duration
+❂ /seekback or /cseekback - Backward Seek the music to your duration
+❂ /restart - Restart bot for your chat .
+
+• Auth User
+❂ /auth [Username] - Add a user to AUTH LIST of the group.
+❂ /unauth [Username] - Remove a user from AUTH LIST of the group.
+❂ /authusers - Check AUTH LIST of the group.
 
 NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
 """
